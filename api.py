d33e20ef4ae81498ed0f3bc56f178beb8be7be10
CharlieGroves <ctrg04@gmail.com>
2021-10-14 21:56:14 +0100

    to laptop

--- api.py
+++ api.py
@@ -1,26 +1,44 @@
 import flask
-from flask import jsonify
-from firestore import getUser, getAllUsers, makeShop
+from flask import jsonify, request
+from firestore import getAllShops, getShop, getUser, getAllUsers, makeShop
 import json
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
+with open("./errors.json") as json_file:
+    errors = json.load(json_file)
 
-@app.route('/', methods=['GET'])
+@app.route('/get', methods=['GET'])
 def getAllUsersRoute():
     data = getAllUsers()
-    return jsonify(data);
+    return jsonify(data)
 
-@app.route('/get/user/<page_id>', methods=['GET'])
-def getUserIdRoute(page_id):
-    user = getUser(page_id)
+@app.route('/get/user/<user_id>', methods=['GET'])
+def getUserIdRoute(user_id):
+    user = getUser(user_id)
     return jsonify(user)
 
-@app.route('/post/user/<user_id>/shop/<shop_name>', methods=['POST'])
-def makeShopRoute(user_id, shop_name):
-    makeShop(user_id, shop_name)
+@app.route('/get/shop/<shop_name>', methods=['GET'])
+def getShopRoute(shop_name):
+    shop = getShop(shop_name);
+    return jsonify(shop)
+
+@app.route("/get/all-shops")
+def getAllShopsRoute():
+    shops = getAllShops()
+    return jsonify(shops)
+
+@app.route('/post/newshop', methods=['POST'])
+def makeShopRoute():
+    data = request.get_json()
+    print(data["user_id"])
+    user_id = data["user_id"]
+    shop_name = data["shop_name"]
+    if makeShop(user_id, shop_name) == 0:
+       return json.dumps({"error": errors["0"]}), 409, {'ContentType':'application/json'}
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
+if __name__ == '__main__':
+    app.run(debug = True, host="0.0.0.0", port=9000, threaded=True)
 
-app.run()
+
--- firestore.py
+++ firestore.py
@@ -1,37 +1,71 @@
+# import database management and authentication modules
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use a service account
+# use a service account to have uninterrupted access to database
 cred = credentials.Certificate('cred/shop-builder-dev-firebase-adminsdk-tgh0r-9e9be1f435.json')
+# authenticate the server as an admin
 firebase_admin.initialize_app(cred)
 
+# establish a connection to the database
 db = firestore.client()
 
+# get all users from the database
 def getAllUsers():
+    # set user arrary to empty
     users = []
+    # create reference to users in database
     users_ref = db.collection('users')
+    # get users from database using reference
     all_users = users_ref.stream()
+    # loop through response
     for user in all_users:
+        # add all users to users array
         users.append(user.to_dict())
+    # return the array of users
     return (users)
 
+# get a single user from their id
 def getUser(uid):
+    # create reference to that user in the database
     user_ref = db.collection('users').document(uid)
+    # get the data in this document
     user = user_ref.get()
+    # return the user
     return (user.to_dict())
 
+# make a new shop
 def makeShop(user_id, shop_name):
+    # create reference to the shop in the database
     shop_ref = db.collection('shops').document(shop_name)
+    # get current data from that location
     shop = shop_ref.get()
+    # if the shop already exists, return 0 which is 
+    # an error because shop names have to be unique
     if shop.exists:
-        raise Exception("Shop already exists")
+        return(0)
+    # otherwise, make a json object with the shop name and the id of the owner of the shop
     data = {
         "shop_name": shop_name,
         "owner_id": user_id,
     }
-    shop_ref.set(data)
-    return 
+    # add this json object to the database
+    return shop_ref.set(data)
+
+def getShop(shop_name):
+    shop_ref = db.collection('shops').document(shop_name)
+    shop = shop_ref.get()
+    return (shop.to_dict())
+
+def getAllShops():
+    shops=[]
+    shops_ref = db.collection('shops')
+    all_shops = shops_ref.stream()
+    for shop in all_shops:
+        shops.append(shop.to_dict())
+    return (shops)
+
 
 getUser("QMKon2v385TLlFwkxGgzc0Il7x52")
 
